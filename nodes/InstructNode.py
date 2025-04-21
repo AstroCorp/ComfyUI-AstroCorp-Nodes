2cc6ba155fae1a87525ef49757683a88a115d600
Alejandro Leal <aleaberm12@gmail.com>
2025-04-21 19:02:57 +0200

    param max_new_tokens in instruct node, add preview textarea

--- nodes/InstructNode.py
+++ nodes/InstructNode.py
@@ -12,7 +12,11 @@ class InstructNode:
                 "input_text": ("STRING", {"multiline": True, "forceInput": True}),
                 "system_role": ("STRING", {"multiline": True}),
                 "model_id": ("STRING", {"default": "Qwen/Qwen2.5-7B-Instruct", "multiline": False}),
+                "max_new_tokens": ("INT", {"default": 5000, "min": 1, "step": 100}),
                 "hf_token": ("STRING", {"default": "", "multiline": False}),
+            },
+            "hidden": {
+                "response_content": ("STRING", {"multiline": True}),
             }
         }
 
@@ -20,11 +24,11 @@ class InstructNode:
     FUNCTION = "execute"
     CATEGORY = "AstroCorp"
 
-    def execute(self, input_text, system_role, model_id, hf_token):
+    def execute(self, input_text, system_role, model_id, max_new_tokens, hf_token):
         if hf_token.strip():
             login(token=hf_token)
             
-        pipe = pipeline("text-generation", model=model_id)
+        pipe = pipeline("text-generation", model=model_id, max_new_tokens=max_new_tokens)
         messages = [
             {"role": "system", "content": system_role},
             {"role": "user", "content": input_text},
@@ -33,7 +37,14 @@ class InstructNode:
         response = outputs[0]["generated_text"][-1]
         content = response["content"]
 
-        return (content,)
+        return {
+            "ui": {
+                "string": [
+                    content,
+                ]
+            },
+            "result": (content,),
+        }
 
 NODE_CLASS_MAPPINGS = {
     "InstructNode": InstructNode
